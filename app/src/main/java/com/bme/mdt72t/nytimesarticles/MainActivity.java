488a8706f7d446898011bdefbe2db36a94db88fa
DESKTOP-CG47D8K\mdt72t <patrikkarolyi@hotmail.com>
2018-04-17 15:38:19 +0200

    adapter (no imageurl from REST API), dummy data

--- app/src/main/java/com/bme/mdt72t/nytimesarticles/MainActivity.java
+++ app/src/main/java/com/bme/mdt72t/nytimesarticles/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bme.mdt72t.nytimesarticles;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,8 +8,13 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.bme.mdt72t.nytimesarticles.adapter.ArticleAdapter;
 import com.bme.mdt72t.nytimesarticles.model.ArticlesPOJO;
+import com.bme.mdt72t.nytimesarticles.model.Result;
 import com.bme.mdt72t.nytimesarticles.network.NYTimesArticleService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,18 +31,45 @@ public class MainActivity extends AppCompatActivity {
     //UI vars
     @BindView(R.id.main_recyclerview)
     RecyclerView recyclerView;
-
+    ArticleAdapter articleAdapter;
+    ArticlesPOJO articlesPOJO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        //getArticles();
+        getArticles();
+        //TODO first run POJO
+
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        //recyclerView.setAdapter();
+        articlesPOJO = new ArticlesPOJO();
+        List<Result> results = new ArrayList<Result>();
 
+        Result result1 = new Result();
+        result1.setTitle("Ausztrál pingvinek");
+        result1.setByline("Pocok Pál");
+        result1.setUrl("http://valami.dx.am/");
+        results.add(result1);
+
+        Result result2 = new Result();
+        result2.setTitle("Görlandi makákók");
+        result2.setByline("Prémes Feri");
+        result2.setUrl("http://valami.dx.am/");
+        results.add(result2);
+
+        Result result3 = new Result();
+        result3.setTitle("Osztrák orszarvúak");
+        result3.setByline("Mackó Lackó");
+        result3.setUrl("http://valami.dx.am/");
+        results.add(result3);
+
+
+        articlesPOJO.setResults(results);
+        articleAdapter= new ArticleAdapter(articlesPOJO,this);
+        recyclerView.setAdapter(articleAdapter);
 
     }
 
@@ -55,8 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call<ArticlesPOJO> call, Response<ArticlesPOJO> response) {
-                ArticlesPOJO articlePOJO = response.body();
-
+                articlesPOJO = response.body();
+                recyclerView.setAdapter(new ArticleAdapter(articlesPOJO,getApplicationContext()));
             }
 
             @Override
